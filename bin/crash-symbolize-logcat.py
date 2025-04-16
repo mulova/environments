4d79b739795be64b2d0127810826acaa3fbe7949
mulova <mulova@gmail.com>
2025-04-16 15:15:38 +0900

    git-tag-rm

--- bin/crash-symbolize-logcat.py
+++ bin/crash-symbolize-logcat.py
@@ -11,6 +11,8 @@ def skip_until_crash(file):
         line = file.readline()
         if not line:
             return
+        if 'E/CRASH' in line:
+            return
         if 'beginning of crash' in line:
             return
 
@@ -28,6 +30,7 @@ def find_lib(lib_name):
     return None
 
 def symbolize_line(line):
+    print (line)
     m = compile.match(line)
     if m:
         no = m.group('no')
@@ -45,6 +48,7 @@ def symbolize(file):
     parse=False
     while True:
         line = file.readline().strip()
+        print (line)
         if not line:
             return
         if symbolize_line(line):
@@ -67,37 +71,57 @@ def input_from_stdin(command):
     rc = process.poll()
     return rc
 
+def set_unity_ver(ver):
+    global UNITY
+    UNITY=f'/Applications/Unity/Hub/Editor/{ver}'
+    print (f'Unity={UNITY}')
+
+def extract_info(file):
+    while True:
+        line = file.readline().strip()
+        if not line:
+            return
+        p = re.compile(r".*E\/CRASH\s+.+Version '(?P<version>[0-9\.f]+) .+CPU '(?P<cpu>[^']+)'")
+        m = p.match(line)
+        if m:
+            set_unity_ver(m.group('version'))
+            return
+        
+def set_ndk():
+    global NDK, NDK_TOOLCHAIN, ARCH, ADDR2LINE, ANDROID_SYMBOL_PATH
+    if args.ndk:
+        NDK=os.environ.get('NDK', None)
+    else:
+        NDK=f'{UNITY}/PlaybackEngines/AndroidPlayer/NDK'
+
+    NDK_TOOLCHAIN=f'{NDK}/toolchains'
+
+    #if args.arch == 32:
+    #    ARCH='armeabi-v7a'
+    #    ADDR2LINE=f'{NDK_TOOLCHAIN}/arm-linux-androideabi-4.9/prebuilt/darwin-x86_64/bin/arm-linux-androideabi-addr2line'
+    #    ANDROID_SYMBOL_PATH=f'{UNITY}/PlaybackEngines/AndroidPlayer/Variations/il2cpp/Release/Symbols'
+    #else:
+    ARCH='arm64-v8a'
+    #ADDR2LINE=f'{NDK_TOOLCHAIN}/aarch64-linux-android-4.9/prebuilt/darwin-x86_64/bin/aarch64-linux-android-addr2line'
+    ADDR2LINE=f'{NDK_TOOLCHAIN}/llvm/prebuilt/darwin-x86_64/bin/llvm-addr2line'
+    ANDROID_SYMBOL_PATH=f'{UNITY}/PlaybackEngines/AndroidPlayer/Variations/il2cpp/Release/Symbols'
+    print (f'ADDR2LINE={ADDR2LINE}')
+
 
 stack_color='\033[93m'
 compile = re.compile(r'.+(?P<no>#[0-9]+) pc (?P<addr>\w+)\s+.+lib(?P<lib>[\w]+)\.so')
 parser = argparse.ArgumentParser(description='Symbolicate crash logs from android logcat')
 parser.add_argument('logfile')#, nargs='?')
-parser.add_argument('--arch', default=64, choices=['32', '64'])
 parser.add_argument('--unityVer', help='unity version such as "2021.1.0f1"')
 parser.add_argument('--ndk', help='ndk path')
 parser.add_argument('--symboldir', help="directory path containing '[armeabi-v7a or arm64-v8a]/lib[...].sym.so'")
+#parser.add_argument('--arch', default=64, choices=['32', '64'])
 args = parser.parse_args()
 
 if args.unityVer:
-    UNITY=f'/Applications/Unity/Hub/Editor/{args.unityVer}'
+    set_unity_ver(args.unityVer)
 else:
     UNITY=os.environ.get('UNITY', None)
-
-if args.ndk:
-    NDK=f'{UNITY}/PlaybackEngines/AndroidPlayer/NDK'
-else:
-    NDK=os.environ.get('NDK', None)
-
-NDK_TOOLCHAIN=f'{NDK}/toolchains'
-
-if args.arch == 32:
-    ARCH='armeabi-v7a'
-    ADDR2LINE=f'{NDK_TOOLCHAIN}/arm-linux-androideabi-4.9/prebuilt/darwin-x86_64/bin/arm-linux-androideabi-addr2line'
-    ANDROID_SYMBOL_PATH=f'{UNITY}/PlaybackEngines/AndroidPlayer/Variations/il2cpp/Release/Symbols'
-else:
-    ARCH='arm64-v8a'
-    ADDR2LINE=f'{NDK_TOOLCHAIN}/aarch64-linux-android-4.9/prebuilt/darwin-x86_64/bin/aarch64-linux-android-addr2line'
-    ANDROID_SYMBOL_PATH=f'{UNITY}/PlaybackEngines/AndroidPlayer/Variations/il2cpp/Release/Symbols'
 
 LOGFILE=args.logfile
 if args.symboldir:
@@ -105,7 +129,8 @@ if args.symboldir:
     
 if LOGFILE and os.path.isfile(LOGFILE):
     with open(LOGFILE, 'r') as file:
-        skip_until_crash(file)
+        #skip_until_crash(file)
+        set_ndk()
         symbolize(file)
 # else:
 #     print(f'Input from adb logcat')
